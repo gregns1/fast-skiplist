9eb9bc83afe6f90ee1d5f1f5a3f1afa55d477f5a
Gregory Newman-Smith <gregory.newmansmith@couchbase.com>
2025-05-23 17:49:46 +0100

    range support for skiplist

--- type.go
+++ type.go
@@ -6,9 +6,16 @@ import (
 )
 
 type SkippedSequenceEntry struct {
-	start     uint64
-	end       uint64
-	timestamp int64
+	Start     uint64
+	End       uint64
+	Timestamp int64
+}
+
+func (s *SkippedSequenceEntry) GetNumSequencesInEntry() int64 {
+	if s.Start == s.End {
+		return 1
+	}
+	return int64(s.End - s.Start + 1)
 }
 
 type elementNode struct {
@@ -17,18 +24,12 @@ type elementNode struct {
 
 type Element struct {
 	elementNode
-	key   uint64
-	value interface{}
+	key SkippedSequenceEntry
 }
 
 // Key allows retrieval of the key for a given Element
-func (e *Element) Key() uint64 {
+func (e *Element) Key() SkippedSequenceEntry {
 	return e.key
-}
-
-// Value allows retrieval of the value for a given Element
-func (e *Element) Value() interface{} {
-	return e.value
 }
 
 // Next returns the following Element or nil if we're at the end of the list.
@@ -37,18 +38,41 @@ func (element *Element) Next() *Element {
 	return element.next[0]
 }
 
-//
-//func (element *Element) IsRange() bool {
-//	return element.value.start != element.value.end
-//}
+func (element *Element) IsRange() bool {
+	return element.key.Start != element.key.End
+}
+
+func (element *Element) IsWithinRange(elem SkippedSequenceEntry) bool {
+	return element.key.Start <= elem.Start && element.key.End >= elem.End
+}
 
 type SkipList struct {
 	elementNode
-	maxLevel       int
-	Length         int
-	randSource     rand.Source
-	probability    float64
-	probTable      []float64
-	mutex          sync.RWMutex
-	prevNodesCache []*elementNode
+	NumSequencesInList int64
+	maxLevel           int
+	Length             int
+	randSource         rand.Source
+	probability        float64
+	probTable          []float64
+	mutex              sync.RWMutex
+	backElem           *Element
+	prevNodesCache     []*elementNode
+}
+
+func (list *SkipList) GetLength() int {
+	list.mutex.RLock()
+	defer list.mutex.RUnlock()
+	return list.Length
+}
+
+func (list *SkipList) GetLastElement() *Element {
+	list.mutex.RLock()
+	defer list.mutex.RUnlock()
+	return list.backElem
+}
+
+func (list *SkipList) GetNumSequencesInList() int64 {
+	list.mutex.RLock()
+	defer list.mutex.RUnlock()
+	return list.NumSequencesInList
 }
